112ea84d72543d59a189985d4addbc11ec81dba0
Ninilac <alach987@yahoo.ca>
2020-02-01 18:18:21 -0500

    Rotate worksgit add .!

--- TropicalPenguinsGlobalGameJam/Assets/Scripts/UI/Tetris/TetrisUIController.cs
+++ TropicalPenguinsGlobalGameJam/Assets/Scripts/UI/Tetris/TetrisUIController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class TetrisUIController : MonoBehaviour
 {
-    public GameObject[] Blocks;
     public int[,] BlocksPos;
     private int[] CursorPos;
 
@@ -23,6 +22,7 @@ public class TetrisUIController : MonoBehaviour
         SetGridUI(new int[,] { { 3, 0 }, { 0, 0 }, { 0 , 3 }, { 3 , 3 } });
         SetBlock(new int[,] { {0,0}, {0,1}, {1,1} }, 0);
         MoveBlock(new int[] { 0, 1 },0);
+        //RotateBlock(true,0);
     }
 
     void OpenUI(int playerId, int[,] blockPos, int[,] builtPos)
@@ -50,14 +50,52 @@ public class TetrisUIController : MonoBehaviour
         UpdateBlockPosValidity();
     }
 
+    public void RotateBlock(bool ClockWise, int PlayerId)
+    {
+        int[,] rotMat;
+        if (ClockWise)
+        {
+            //Clockwise
+            rotMat = new int[,] { { 0, -1 }, { 1, 0 } };
+        }
+        else
+        {
+            //AntiClockwise
+            rotMat = new int[,] { { 0, 1 }, { -1, 0 } };
+        }
+
+        for(int i = 0; i < BlocksPos.GetLength(0); ++i)
+        {
+            int[] vec = new int[] { BlocksPos[i, 0], BlocksPos[i, 1] };
+            vec = RotateIntVector(vec, rotMat);
+            BlocksPos[i, 0] = vec[0];
+            BlocksPos[i, 1] = vec[1];
+        }
+        BlockView.DrawBlock(CursorPos, BlocksPos);
+        UpdateBlockPosValidity();
+    }
+
+    private int[] RotateIntVector(int[] vec, int[,] mat)
+    {
+        int[] resVec = new int[2];
+        resVec[0] = vec[0] * mat[0, 0] + vec[1] * mat[0, 1];
+        resVec[1] = vec[0] * mat[1, 0] + vec[1] * mat[1, 1];
+        return resVec;
+    }
+
     public bool UpdateBlockPosValidity()
     {
         bool[,] gridIsEmpty = GridUI.gridIsEmpty;
         bool oldValidity = isBlockPosValid;
         isBlockPosValid = true;
-        for (int i = 0; i < BlocksPos.GetLength(0); ++i)
+        for (int i = 0; i < BlocksPos.GetLength(0) && isBlockPosValid; ++i)
         {
-            if(!gridIsEmpty[CursorPos[0] + BlocksPos[i,0], CursorPos[1] + BlocksPos[i, 1]])
+            int posX = CursorPos[0] + BlocksPos[i, 0];
+            int posY = CursorPos[1] + BlocksPos[i, 1];
+            if (posX < 0 || posY < 0 || posX > GridUI.GridSize || posY > GridUI.GridSize)
+            {
+                isBlockPosValid = false;
+            }else if(!gridIsEmpty[posX, posY])
             {
                 isBlockPosValid = false;
             }

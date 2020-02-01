63d94d57df6a9a2229290aa54699c01299eb5e89
paindeviande <louis.philippe.geoffrion@hotmail.com>
2020-01-31 22:08:00 -0500

    Added LP scene, GameEvents + Utility files

---
+++ TropicalPenguinsGlobalGameJam/Assets/Scripts/Managers/EventManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : Singleton<EventManager>
+{
+	// EXEMPLE EVENTS
+
+	public static GameEvent _ExampleEvent = new GameEvent();
+	public static GameEvent<int> _ExampleIntEvent = new GameEvent<int>();
+	public static GameEvent<Vector3, Quaternion, float, bool, System.Action, Matrix4x4, ushort, double> _ExampleRidiculousParametersEvent = new GameEvent<Vector3, Quaternion, float, bool, System.Action, Matrix4x4, ushort, double>();
+
+	// EXAMPLE EVENTS
+
+
+}

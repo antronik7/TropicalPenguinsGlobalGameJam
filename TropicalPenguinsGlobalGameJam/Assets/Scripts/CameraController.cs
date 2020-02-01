0452a157142728f18df4fa91d09e5164e8a03331
antronik7 <antoineberthiaumedutil@gmail.com>
2020-02-01 18:33:39 -0500

    Camera Shake

--- TropicalPenguinsGlobalGameJam/Assets/Scripts/CameraController.cs
+++ TropicalPenguinsGlobalGameJam/Assets/Scripts/CameraController.cs
@@ -4,23 +4,22 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public float durationShake = 1f;
+    public float magnitudeShake = 1f;
     public float m_DampTime = 0.2f;                 // Approximate time for the camera to refocus.
     public float m_ScreenEdgeBuffer = 4f;           // Space between the top/bottom most target and the screen edge.
     public float m_MinSize = 6.5f;                  // The smallest orthographic size the camera can be.
     public Transform[] m_Targets; // All the targets the camera needs to encompass.
-
 
     private Camera m_Camera;                        // Used for referencing the camera.
     private float m_ZoomSpeed;                      // Reference speed for the smooth damping of the orthographic size.
     private Vector3 m_MoveVelocity;                 // Reference velocity for the smooth damping of the position.
     private Vector3 m_DesiredPosition;              // The position the camera is moving towards.
 
-
     private void Awake()
     {
         m_Camera = GetComponentInChildren<Camera>();
     }
-
 
     private void FixedUpdate()
     {
@@ -31,7 +30,6 @@ public class CameraController : MonoBehaviour
         Zoom();
     }
 
-
     private void Move()
     {
         // Find the average position of the targets.
@@ -40,7 +38,6 @@ public class CameraController : MonoBehaviour
         // Smoothly transition to that position.
         transform.position = Vector3.SmoothDamp(transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);
     }
-
 
     private void FindAveragePosition()
     {
@@ -70,14 +67,12 @@ public class CameraController : MonoBehaviour
         m_DesiredPosition = averagePos;
     }
 
-
     private void Zoom()
     {
         // Find the required size based on the desired position and smoothly transition to that size.
         float requiredSize = FindRequiredSize();
         m_Camera.orthographicSize = Mathf.SmoothDamp(m_Camera.orthographicSize, requiredSize, ref m_ZoomSpeed, m_DampTime);
     }
-
 
     private float FindRequiredSize()
     {
@@ -116,7 +111,6 @@ public class CameraController : MonoBehaviour
         return size;
     }
 
-
     public void SetStartPositionAndSize()
     {
         // Find the desired position.
@@ -127,5 +121,38 @@ public class CameraController : MonoBehaviour
 
         // Find and set the required size of the camera.
         m_Camera.orthographicSize = FindRequiredSize();
+    }
+
+    public void StartShake()
+    {
+        StartCoroutine(Shake());
+    }
+
+    IEnumerator Shake()
+    {
+        float elapsed = 0.0f;
+
+        Vector3 originalCamPos = Camera.main.transform.localPosition;
+
+        while (elapsed < durationShake)
+        {
+
+            elapsed += Time.deltaTime;
+
+            float percentComplete = elapsed / durationShake;
+            float damper = 1.0f - Mathf.Clamp(4.0f * percentComplete - 3.0f, 0.0f, 1.0f);
+
+            // map value to [-1, 1]
+            float x = Random.value * 2.0f - 1.0f;
+            float y = Random.value * 2.0f - 1.0f;
+            x *= magnitudeShake * damper;
+            y *= magnitudeShake * damper;
+
+            Camera.main.transform.localPosition = new Vector3(x, y, originalCamPos.z);
+
+            yield return null;
+        }
+
+        Camera.main.transform.localPosition = originalCamPos;
     }
 }
